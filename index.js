dfd17c58dd209e0c49535cb922c35aabcd48729c
Barry Porch <bporch@ymail.com>
2021-01-14 14:57:38 -0500

    Completed Task 2

--- index.js
+++ index.js
@@ -40,9 +40,13 @@ Use getFinals to do the following:
 hint - you should be looking at the stage key inside of the objects
 */
 
-function getFinals(/* code here */) {
-   /* code here */
-}
+function getFinals(data) {
+    const final = fifaData.filter((game) => {
+      return game.Stage === "Final"
+    })
+  
+    return final
+  }
 
 
 
